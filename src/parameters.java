2d5c67c032cb67b1cb77896fe9f33e4263bc7a08
manuelduarte077 <carlosd99m@gmail.com>
2021-01-01 19:08:36 -0600

    arrays with parameters

---
+++ src/parameters.java
@@ -0,0 +1,23 @@
+public class parameters {
+    /**
+     * Arreglos con parametros
+     */
+
+    public static void utilizarArreglo(int numeros[]) {
+        numeros[4] = 0;
+    }
+
+    public static void main (String[] args) {
+        int numeros[] = {1, 2, 3, 4, 5};
+        utilizarArreglo(numeros);
+
+        System.out.println(numeros[2]);
+
+        // Otra forma de recorrer los for
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+
+    }
+
+}
