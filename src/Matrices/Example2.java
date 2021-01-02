883d8658766a03ee36fcaf30b39631fc33942fd8
manuelduarte077 <carlosd99m@gmail.com>
2021-01-01 20:03:24 -0600

    Array de 3 dimensiones

---
+++ src/Matrices/Example2.java
@@ -0,0 +1,29 @@
+package Matrices;
+public class Example2 {
+
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 3;
+        int z = 3;
+
+        String [] [] [] cubo = new String[x][y][z];
+        for (int i = 0; i < x; i++ ) {
+            for (int j = 0; j < y; j++) {
+                for (int k = 0; k < z; k++ ) {
+                    cubo[i][j][k] = String.valueOf(i) +
+                                    String.valueOf(j) +
+                                    String.valueOf(k);
+                }
+            }
+        }
+        for (int i = 0; i < x; i++ ) {
+            for (int j = 0; j < y; j++) {
+                for (int k = 0; k < z; k++ ) {
+                    System.out.println(cubo[i] [j] [k]);
+                }
+            }
+        }
+
+    }
+
+}
