2dd8389dfa9e1d86d46c1afcad2bb87b80730df5
manuelduarte077 <carlosd99m@gmail.com>
2021-01-01 18:57:52 -0600

    Arreglos

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        int arreglo[];
+
+        int numeros[] = new int[10]; // inicializando el arreglo, en 30
+        numeros[9] = 1;
+        numeros[8] = 2;
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+    }
+
+}
